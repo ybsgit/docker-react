3b78f334c753cd3b527818bee7c5c7241129b94c
root <root@ip-172-31-40-213.ec2.internal>
2019-03-11 15:16:16 +0000

    feature

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
           Hi there!!!!!
+	  Feature branch.
 	  </p>
           <a
             className="App-link"
